48dae979887a52bd482a92c66d5c7d026c8ede0a
johndyer24 <johndyer24@gmail.com>
2019-02-04 22:47:21 -0500

    complete example project

---
+++ electron/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const url = require('url');
+const { channels } = require('../src/shared/constants');
+
+let mainWindow;
+
+function createWindow () {
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '../index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL(startUrl);
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on(channels.APP_INFO, (event) => {
+  event.sender.send(channels.APP_INFO, { 
+    appName: app.getName(),
+    appVersion: app.getVersion(),
+  });
+});
